6645e4befa75d8e79f74c52afdfa2ac90c3639f5
Berke Karaca <berkekaraca54@gmail.com>
2025-06-09 15:48:15 +0300

    Gereksiz ve hassas dosyalar Git’ten kaldirildi, .gitignore guncellendi

--- test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Merhaba, Flask çalışıyor!'
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
